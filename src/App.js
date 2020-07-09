653972268178c1a1ba0d5f7bd518ab7a6e9de720
aamirali <sufaamir@gmail.com>
2020-07-09 12:42:13 -0400

    update apps components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import { Cards, CountryPicker, Chart } from './components'
+import styles from './App.module.css'
+import { fetchData } from './components/api/index'
+import coronaImg from './images/a.jpg'
+import Footer from './Footer'
+class App extends Component {
 
-function App() {
-  return (
-    <>
-      Aamir Ali COVID19
-    </>
-  );
+  state = {
+    data: {},
+    country: ''
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({
+      data: fetchedData
+    })
+  }
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country })
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+
+        <img className={styles.image} src={coronaImg} alt='COVID-19' />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+        <Footer />
+      </div>
+
+    )
+  }
 }
 
-export default App;
+export default App
 
 
 //lackadaisical-lunchroom.surge.sh
