33d481c565a66cad67e9f4893b8d14616a0c333f
aamirali <sufaamir@gmail.com>
2020-07-09 05:40:58 -0400

    add new project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      Aamir Ali
+    </>
+  );
+}
+
+export default App;
+
+
+//lackadaisical-lunchroom.surge.sh
