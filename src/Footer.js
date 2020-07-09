d22cf7d5f2ce1643e5f679d9e98d5b834353ceb6
aamirali <sufaamir@gmail.com>
2020-07-09 12:43:01 -0400

    update app

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { CenterFocusStrong } from '@material-ui/icons'
+
+function Footer() {
+  return (
+
+    <footer class="page-footer" style={{ width: '100%' }}>
+      <div class="footer-copyright">
+        <div class="container">
+          --------------------------------------------Aamir Ali (Roll No CNC021762)-----------------------------------------------
+            </div>
+      </div>
+    </footer>
+
+  )
+}
+
+export default Footer
